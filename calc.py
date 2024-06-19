87ad3d70ccea819052794cd58359eaa8eea526b3
Dhivyasri22 <dhivyasri22122003@gmail.com>
2024-06-19 19:04:29 +0530

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,18 @@
+a=int(input("A:"))
+b=int(input("B:"))
+operation=input("add/sub/mul/div:")
+if(operation=="add"):
+   print(a+b)
+elif(operation=="sub"):
+   print(a-b)
+elif(operation=="mul"):
+   print(a*b)
+elif(operation=="div"):
+   print(a/b)
+else:
+   print("invalid operator")
+
+#A:10
+#B:20
+#add/sub/mul/div:add
+#30
